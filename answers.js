d828061e80b5c34fde29f3e06ee48a92305100a6
GregoryFontana <gregjfontana@gmail.com>
2022-09-13 23:03:29 -0400

    Methods Revisited Answered

--- answers.js
+++ answers.js
@@ -52,6 +52,22 @@ const ninTurts = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
 ////////////////////////////////
 // Methods, Revisited
 ////////////////////////////////
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+
+console.log(favMovies[8])
+console.log(favMovies.sort()) //The method rearranged them in ascending alphabetical order.
+console.log(favMovies.pop())
+console.log(favMovies.push("Guardians of the Galaxy"))
+console.log(favMovies.reverse())
+console.log(favMovies.shift())
+console.log(favMovies.unshift()) //It returns "18" because there are no parameters to insert into he new array.
+favMovies.splice(14, 0, 'Avatar')
+console.log(favMovies) // The array has been altered.
+console.log(favMovies.slice(8,18)) // .slice does not alter the original array.
+let newSlice = favMovies.slice(8,18)
+console.log(newSlice) // Not quite sure. It returns exactly what I expected.
+console.log(favMovies.indexOf('Fast and Furious'))// We get 13. I don't know why.
+//Not weird. We cannot change the value of a variable declared with const but we can change its contents. 
 
 ////////////////////////////////
 // Where is Waldo
